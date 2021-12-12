905b70643a0e2933b9e8227bb0087d2819fd8bad
Curt Brune <curt@brune.net>
2021-12-12 12:00:54 -0800

    WIP: first stab at getting server working
    it works, but requires a specific type of shutdown future
    
    Signed-off-by: Curt Brune <curt@brune.net>

--- tower-ex/src/main.rs
+++ tower-ex/src/main.rs
@@ -12,6 +12,7 @@
 // where F: Future<Output = ()>,
 
 use tokio::runtime::Builder;
+use tower::{Service, ServiceExt};
 
 use sample::prelude::*;
 
@@ -30,7 +31,45 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let err = AppError::SampleError("for grins".to_owned());
         println!("Err: {:?}", err);
 
-        // let svc = SampleService::new();
+        let mut svc = SampleService::new();
+
+        let transceiver1 = SampleTransceiver::new(1, 3, 5);
+        let transceiver2 = SampleTransceiver::new(100, 3, 5);
+
+        // A shutdown signal
+        let (tx, rx) = tokio::sync::oneshot::channel();
+
+        let server = Server::builder()
+            .with_transceiver(transceiver1)
+            .with_transceiver(transceiver2)
+            .serve_with_shutdown(svc, rx);
+
+        // create a task to trigger shutdown in the future
+        tokio::spawn(async move {
+            for i in 0..7 {
+                println!("shutdown task: {}: sleeping ...", i);
+                tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+            }
+            println!("shutdown task: shutting down ...");
+            let _ = tx.send(());
+        });
+
+        println!("main: waiting for server to shutdown ...");
+        match server.await {
+            Ok(_) => println!("Server exits success"),
+            Err(_) => println!("Server exits error"),
+        }
+
+        /*
+                for i in 0..3 {
+                    let request = SampleRequest::new(i);
+                    println!("request: {:?}", request);
+                    let response = svc.ready().await.unwrap().call(request).await.unwrap();
+
+                    println!("response: {:?}", response);
+                    println!("service: {:?}", svc);
+                }
+        */
     });
 
     Ok(())
--- tower-ex/src/server.rs
+++ tower-ex/src/server.rs
@@ -1,1 +1,71 @@
 //! Application Server
+
+// server is a  future
+
+use std::fmt;
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+
+use tokio::sync::oneshot::Receiver;
+
+use crate::prelude::*;
+
+pub struct Server<S, T> {
+    service: S,
+    transceivers: Vec<T>,
+    signal: Pin<Box<Receiver<()>>>,
+}
+
+#[derive(Debug)]
+pub struct Builder<T> {
+    transceivers: Vec<T>,
+}
+
+impl<T> Server<(), T> {
+    pub fn builder() -> Builder<T> {
+        Builder {
+            transceivers: Vec::new(),
+        }
+    }
+}
+
+impl<S, T> Unpin for Server<S, T> {}
+
+impl<S, T> Future for Server<S, T> {
+    type Output = Result<(), AppError>;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        println!("Starting server poll loop....");
+        loop {
+            // any transceivers have work?
+            // received shutdown signal?
+            let signal = Pin::as_mut(&mut self.signal);
+            match signal.poll(cx) {
+                Poll::Ready(_result) => {
+                    println!("Received shutdown signal");
+                    return Poll::Ready(Ok(()));
+                }
+                Poll::Pending => {
+                    println!("poll: Returning Pending");
+                    return Poll::Pending;
+                }
+            }
+        }
+    }
+}
+
+impl<T> Builder<T> {
+    pub fn with_transceiver(mut self, transceiver: T) -> Builder<T> {
+        self.transceivers.push(transceiver);
+        self
+    }
+
+    pub fn serve_with_shutdown<S>(self, service: S, signal: Receiver<()>) -> Server<S, T> {
+        Server {
+            service,
+            transceivers: self.transceivers,
+            signal: Box::pin(signal),
+        }
+    }
+}
